30c7abf080e56b3a39740cc58fd5c79e8acd169b
Jonathan Wright <jdwright@ldc.upenn.edu>
2023-08-01 15:28:03 -0400

    tsv2textgrid

--- bin/ldclblx.dart
+++ bin/ldclblx.dart
@@ -1,5 +1,41 @@
+import 'dart:io';
+import 'package:args/args.dart';
+import 'package:path/path.dart' as p;
 import 'package:ldclblx/ldclblx.dart' as ldclblx;
 
+const lineNumber = 'line-number';
+
 void main(List<String> arguments) {
-  print('Hello world: ${ldclblx.calculate()}!');
+  // print('Hello world: ${ldclblx.calculate()}!');
+  final parser = ArgParser()..addFlag(lineNumber, negatable: false, abbr: 'n');
+  ArgResults argResults = parser.parse(arguments);
+  final fns = argResults.rest;
+  final f = File(fns[0]);
+  if (f.existsSync()) {
+    var type = whichFormat(fns[0]);
+    var out = File(fns[1]);
+    switch (type) {
+      case 'tsv':
+        out.writeAsStringSync(ldclblx.tsv(f.readAsLinesSync()));
+    }
+  } else {
+    stdout.writeln("file doesn't exist");
+  }
+}
+
+Future<void> _handleError(String path) async {
+  if (await FileSystemEntity.isDirectory(path)) {
+    stderr.writeln('error: $path is a directory');
+  } else {
+    exitCode = 2;
+  }
+}
+
+String whichFormat(fn) {
+  final ext = p.extension(fn);
+  if (ext == '.tsv') {
+    return 'tsv';
+  } else {
+    return 'none';
+  }
 }
--- lib/ldclblx.dart
+++ lib/ldclblx.dart
@@ -1,3 +1,40 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+import 'dart:convert';
+
+String tsv(x) {
+  final lines = x; //utf8.decoder.bind(x).transform(const LineSplitter());
+  String header1 = '''File type = "ooTextFile"
+Object class = "TextGrid"
+0
+''';
+  String header2 = '''<exists>
+1
+"IntervalTier"
+"segment"
+0
+''';
+  // print(out);
+  String out = "";
+
+  String header3 = "";
+  var first = lines[1].split("\t");
+  String last = '';
+  if (double.parse(first[2]) > 0) {
+    header3 = "${lines.length + 2}\n";
+    last = '0';
+  } else {
+    header3 = "${lines.length + 1}\n";
+  }
+  List a = [];
+  for (final line in lines.sublist(1)) {
+    a = line.split("\t");
+    if (double.parse(first[2]) > 0) {
+      out = "$out$last\n${a[2]}\n\"\"\n";
+    }
+    out = "$out${a[2]}\n${a[3]}\n\"${a[4]}\"\n";
+    last = a[3];
+  }
+  var end = (double.parse(a[3]) + 1.0).toString();
+  out = "$header1$end\n$header2$end\n$header3$out${a[3]}\n$end\n\"\"\n";
+  return out;
 }
